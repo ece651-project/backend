71c3ea8e3b5990a18306e2afcf9cdf9ffa948fa5
Zishuo Xu <xuzishuo1996@gmail.com>
2021-01-29 12:53:14 +0800

    add Jackson (a json mapper) config. write new addUser with json request and response
    create new user controller test after using jackson

--- src/main/java/com/project/ece651/webapp/controllers/UserController.java
+++ src/main/java/com/project/ece651/webapp/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.project.ece651.webapp.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.project.ece651.webapp.models.CreateUserRequestModel;
 import com.project.ece651.webapp.models.CreateUserResponseModel;
+import com.project.ece651.webapp.models.ErrorResponse;
 import com.project.ece651.webapp.services.UserService;
 import com.project.ece651.webapp.shared.UserDto;
 import org.modelmapper.ModelMapper;
@@ -13,83 +17,56 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 /*TODO:
     1. change the html file name to be consistent with frontend
-    2. add update and delete controllers
+    2. add update and delete controller methods
     3. login
 */
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    private static final String USER_FORM_URL = "users/userform";   // resources/templates/users/userform.html
-
     private final UserService userService;
+    private final ObjectMapper jsonMapper;
     private final ModelMapper modelMapper;
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public UserController(UserService userService, ModelMapper modelMapper) {
+    public UserController(UserService userService, ObjectMapper jsonMapper, ModelMapper modelMapper) {
         this.userService = userService;
+        this.jsonMapper = jsonMapper;
 //        modelMapper = new ModelMapper();
 //        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         this.modelMapper = modelMapper;
     }
 
-    @GetMapping("/add_user")
-    public String addUser(Model model){
-        model.addAttribute("user", new CreateUserRequestModel());
-
-        return USER_FORM_URL;
-    }
-
-    @PostMapping()  // TODO: post URL may change to be consistent with frontend
-    public String createUser(@Valid @ModelAttribute("user") CreateUserRequestModel createUserRequestModel,
-                             BindingResult bindingResult) {
-
-        // for user data input validation
-        if (bindingResult.hasErrors()) {
-
-            bindingResult.getAllErrors().forEach(objectError -> {
-                logger.info(objectError.toString());
-            });
-
-            return USER_FORM_URL;
-        }
-
-        UserDto userDto = modelMapper.map(createUserRequestModel, UserDto.class);
-        UserDto createdUserDto = userService.createUser(userDto);
-
-        return "redirect:/user/" + createdUserDto.getUid();
-    }
-
-    // for Postman: could run without frontend
-    @PostMapping(value = "/add_user-postman",
+    /* could use createUserRequestModel as RequestBody
+       if we need backend to validate input data:
+       use @Valid and BindingResult
+     */
+    @PostMapping(value = "/add_user",
             consumes = {MediaType.APPLICATION_JSON_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE}
-    )
-    public ResponseEntity<CreateUserResponseModel> createUserPostman(@Valid @RequestBody CreateUserRequestModel userDetails) {
-
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
-        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
-        UserDto createdUser = userService.createUser(userDto);
-
-        CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
-    }
-
-    @GetMapping("/{userId}")
-    public String showById(@PathVariable String userId, Model model) {
-
-        model.addAttribute("user", userService.getUserByUserId(userId));
-
-        return "users/show";
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    @ResponseBody
+    public String addUser(@RequestBody String userJson) throws JsonProcessingException {
+        try {
+            UserDto userDto = jsonMapper.readValue(userJson, UserDto.class);
+            UserDto createdUserDto = userService.createUser(userDto);
+            return jsonMapper.writeValueAsString(createdUserDto.getUid());
+        } catch (Exception e) {
+            // Assumes that other constrains including length and email format have been checked by frontend
+            // only consider uniqueness here
+            String errMsg = e.getMessage();
+            if (errMsg.contains("EMAIL")) {
+                errMsg = "You have already signed up an account with this email!";
+            } else if (errMsg.contains("NICKNAME")) {
+                errMsg = "This nickname has already been used! Please Change another one!";
+            }
+            return jsonMapper.writeValueAsString(new ErrorResponse(false, errMsg));
+        }
     }
 }
+
--- src/test/java/com/project/ece651/webapp/controllers/UserControllerThymeleafTest.java
+++ src/test/java/com/project/ece651/webapp/controllers/UserControllerThymeleafTest.java
@@ -1,6 +1,5 @@
 package com.project.ece651.webapp.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.ece651.webapp.services.UserService;
 import com.project.ece651.webapp.shared.UserDto;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,19 +11,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class UserControllerTest {
+class UserControllerThymeleafTest {
 
     @Mock
     UserService userService;
 
-    UserController controller;
+    UserControllerThymeleaf controller;
 
     MockMvc mockMvc;
 
@@ -34,37 +32,29 @@ class UserControllerTest {
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        ObjectMapper jsonMapper = new ObjectMapper();
-        controller = new UserController(userService, jsonMapper, modelMapper);
+        controller = new UserControllerThymeleaf(userService, modelMapper);
 
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
-    /* ------------------ addUser() tests ---------------------- */
-    // cover different branches of addUser() method
+    // for Postman
     @Test
-    void testAddUserSuccess() throws Exception {
+    void createUserPostman() throws Exception {
         UserDto userDto = new UserDto();
-        String expectedUid = "0d2adfec-1ce9-483c-a1e7-59d31df";
-        userDto.setUid(expectedUid);
-        userDto.setNickname("aaaaaaaa");
-        userDto.setEmail("aaaaaa@163.com");
-        userDto.setPhoneNum("1111111111111");
-        userDto.setPassword("11111111");
-        userDto.setEncryptedPassword("$$$$$");
+        userDto.setNickname("Jack");
 
         when(userService.createUser(any())).thenReturn(userDto);
 
-        // in JSON format: the content does not matter
-        String userRequestBody = "{\"nickname\":\"bbbbbbbb\",\"email\":\"bbbbbb@163.com\",\"password\":\"22222222\", \"phoneNum\":\"12345678910\"}";
+        // in JSON format
+        String userRequestBody = "{\"nickname\":\"aaaaaaaa\",\"email\":\"aaaaaa@163.com\",\"password\":\"11111111\", \"phoneNum\":\"12345678910\"}";
 
-        String res = mockMvc.perform(post("/user/add_user")
+        mockMvc.perform(post("/user/add_user-postman")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(userRequestBody)
         )
+                .andExpect(status().isCreated())
                 .andReturn().getResponse().getContentAsString();
-        String resUid = res.substring(1, res.length() - 1); // remove the leading and trailing double quote
-
-        assertEquals(expectedUid, resUid);
     }
+
+    // TODO: add tests
 }
