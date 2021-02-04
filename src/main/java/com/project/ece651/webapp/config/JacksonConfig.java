f93b499e800bd33dab1cd774e3060e9802a09789
Zishuo Xu <xuzishuo1996@gmail.com>
2021-02-04 16:25:53 +0800

    modify JSON mapper config for enum

--- src/main/java/com/project/ece651/webapp/config/JacksonConfig.java
+++ src/main/java/com/project/ece651/webapp/config/JacksonConfig.java
@@ -18,7 +18,7 @@ public class JacksonConfig {
         mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         // (official) https://github.com/FasterXML/jackson-databind/wiki/Serialization-Features
         // https://stackoverflow.com/questions/18031125/what-is-the-difference-between-enum-name-and-enum-tostring
-        mapper.configure(WRITE_ENUMS_USING_TO_STRING, true);
+        // mapper.configure(WRITE_ENUMS_USING_TO_STRING, true); // incurs inconsistency
         return mapper;
     }
 }
